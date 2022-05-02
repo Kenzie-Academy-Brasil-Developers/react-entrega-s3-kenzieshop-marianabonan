b6af2d04d31366f677f75f228398ed76540a8c00
marianabonan <marianabonanbs@gmail.com>
2022-05-02 17:53:04 -0300

    feat: add bibliotecas, pastas e produtos

---
+++ src/store/modules/user/reducer.js
@@ -0,0 +1,68 @@
+const produtos = [
+  {
+    name: "Coxinha de catnip",
+    price: 32.9,
+    image:
+      "https://cdn.awsli.com.br/1000x1000/31/31979/produto/29003813/f0b1547824.jpg",
+  },
+  {
+    name: "Catnip | Erva de gato",
+    price: 11.9,
+    image:
+      "https://cdn.awsli.com.br/1000x1000/31/31979/produto/29120347/c9188371b3.jpg",
+  },
+  {
+    name: "Sardinha de catnip em lata",
+    price: 58.9,
+    image:
+      "https://cdn.awsli.com.br/1000x1000/31/31979/produto/73488994/bcd23938c5.jpg",
+  },
+  {
+    name: "Fantasia de dinossauro verde",
+    price: 99.9,
+    image:
+      "https://cdn.awsli.com.br/1000x1000/31/31979/produto/29095759/c530bedfc7.jpg",
+  },
+  {
+    name: "Moletom cinza e azul",
+    price: 89.9,
+    image:
+      "https://cdn.awsli.com.br/1000x1000/31/31979/produto/29119783/c27b6209c2.jpg",
+  },
+  {
+    name: "Fantasia juba de leão",
+    price: 59.9,
+    image:
+      "https://cdn.awsli.com.br/1000x1000/31/31979/produto/29095720/278a028096.jpg",
+  },
+  {
+    name: "Fantasia de unicórnio",
+    price: 59.9,
+    image:
+      "https://cdn.awsli.com.br/1000x1000/31/31979/produto/29004299/ce071dd7f0.jpg",
+  },
+  {
+    name: "Fantasia de vaca",
+    price: 99.9,
+    image:
+      "https://cdn.awsli.com.br/1000x1000/31/31979/produto/32363745/774da1dfb9.jpg",
+  },
+  {
+    name: "Castelo papelão",
+    price: 2.0,
+    image:
+      "https://cdn.awsli.com.br/1000x1000/31/31979/produto/56581820/e7ca48f848.jpg",
+  },
+  {
+    name: "Tv tubo de papelão",
+    price: 59.9,
+    image:
+      "https://cdn.awsli.com.br/1000x1000/31/31979/produto/66773634/dc96176802.jpg",
+  },
+  {
+    name: "Nave espacial de papelão",
+    price: 59.9,
+    image:
+      "https://cdn.awsli.com.br/1000x1000/31/31979/produto/57597261/c3258574c0.jpg",
+  },
+];
